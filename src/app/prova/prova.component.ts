fd76a375c59a4ae399759fd2b8ca9ffbae504e66
elperez365 <ceti.vasco@gmail.com>
2023-08-08 13:25:06 +0200

    lifecycles loaded

--- src/app/prova/prova.component.ts
+++ src/app/prova/prova.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-prova',
   templateUrl: './prova.component.html',
-  styleUrls: ['./prova.component.css']
+  styleUrls: ['./prova.component.css'],
 })
-export class ProvaComponent {
+export class ProvaComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentChecked,
+    AfterContentInit,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  cani = [
+    {
+      nome: 'roger',
+      razza: 'golden',
+      descrizione: 'questa è la descrizione',
+    },
+  ];
 
+  constructor() {
+    console.log('costruttore');
+  }
+  ngOnInit(): void {
+    console.log('ngOninit');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
 }
